aa5af11f33f11abce864d18a0695b5bc9b73ee1e
Deploy Agent <deploy@example.com>
2026-05-04 13:53:33 +0530

    Apply DOMMatrix fix at the Netlify function entry point

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,3 +1,11 @@
+// Polyfills for pdf-parse in serverless environments
+if (typeof global.DOMMatrix === 'undefined') {
+    global.DOMMatrix = class DOMMatrix {};
+}
+if (typeof global.Path2D === 'undefined') {
+    global.Path2D = class Path2D {};
+}
+
 const serverless = require('serverless-http');
 const app = require('../../server/index');
 
